03c785785d476087ba10d45e5c7183db67cab49a
Lucia Escanellas <raviolicode@gmail.com>
2015-02-10 04:58:05 -0200

    I can see graphics by data fields

---
+++ app.js
@@ -0,0 +1,140 @@
+'use strict';
+
+var myapp = angular.module('myapp', ["highcharts-ng"]);
+
+myapp.controller('myctrl', function ($scope, $http) {
+  //atuservicio-rails:3000/departamento/montevideo.json
+  $http.get('montevideo.json').then(function(res) {
+    var data = res.data;
+    $scope.providers = res.data;
+
+    $scope.chartTypes = [
+      {"id": "line", "title": "Line"},
+      {"id": "spline", "title": "Smooth line"},
+      {"id": "area", "title": "Area"},
+      {"id": "areaspline", "title": "Smooth area"},
+      {"id": "column", "title": "Column"},
+      {"id": "bar", "title": "Bar"},
+      {"id": "pie", "title": "Pie"},
+      {"id": "scatter", "title": "Scatter"}
+    ];
+
+    $scope.dashStyles = [
+      {"id": "Solid", "title": "Solid"},
+      {"id": "ShortDash", "title": "ShortDash"},
+      {"id": "ShortDot", "title": "ShortDot"},
+      {"id": "ShortDashDot", "title": "ShortDashDot"},
+      {"id": "ShortDashDotDot", "title": "ShortDashDotDot"},
+      {"id": "Dot", "title": "Dot"},
+      {"id": "Dash", "title": "Dash"},
+      {"id": "LongDash", "title": "LongDash"},
+      {"id": "DashDot", "title": "DashDot"},
+      {"id": "LongDashDot", "title": "LongDashDot"},
+      {"id": "LongDashDotDot", "title": "LongDashDotDot"}
+    ];
+
+
+    // $scope.chartStack = [
+    //   {"id": '', "title": "No"},
+    //   {"id": "normal", "title": "Normal"},
+    //   {"id": "percent", "title": "Percent"}
+    // ];
+
+    // $scope.addPoints = function () {
+    //   var seriesArray = $scope.chartConfig.series;
+    //   var rndIdx = Math.floor(Math.random() * seriesArray.length);
+    //   seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+    // };
+
+    // $scope.addSeries = function () {
+    //   var rnd = []
+    //   for (var i = 0; i < 10; i++) {
+    //     rnd.push(Math.floor(Math.random() * 20) + 1)
+    //   }
+    //   $scope.chartConfig.series.push({
+    //     data: rnd
+    //   })
+    // }
+
+//     $scope.removeRandomSeries = function () {
+//       var seriesArray = $scope.chartConfig.series;
+//       var rndIdx = Math.floor(Math.random() * seriesArray.length);
+//       seriesArray.splice(rndIdx, 1)
+//     }
+//
+    // $scope.removeSeries = function (id) {
+    //   var seriesArray = $scope.chartConfig.series;
+    //   seriesArray.splice(id, 1)
+    // }
+
+    // $scope.toggleHighCharts = function () {
+    //   this.chartConfig.useHighStocks = !this.chartConfig.useHighStocks
+    // }
+
+    // $scope.replaceAllSeries = function () {
+    //   var data = [
+    //     { name: "first", data: [10] },
+    //     { name: "second", data: [3] },
+    //     { name: "third", data: [13] }
+    //   ];
+    //   $scope.chartConfig.series = data;
+    // };
+    //
+
+    $scope.fields = Object.keys($scope.providers[0]);
+
+    $scope.providers_names = $scope.providers.map(function(prov) {
+      return prov.nombre_abreviado;
+    });
+
+    $scope.getAllByProp = function(prop) {
+      return $scope.providers.map(function(prov) { return parseFloat(prov[prop]); });
+    }
+
+    $scope.chartSeries = [
+      { "name": "afiliados", "data": $scope.getAllByProp("afiliados") }
+    ];
+
+    $scope.addSeries = function(name) {
+      $scope.chartConfig.yAxis.push({
+        labels: {
+          format: '{value} %',
+          title: name
+        }
+      });
+
+      $scope.chartConfig.series.push({
+        "name": name,
+        "data": $scope.getAllByProp(name)
+      });
+    }
+
+   $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'areaspline',
+                zoomType: 'xy'
+            }
+        },
+        series: $scope.chartSeries,
+        title: {
+            text: 'Comparador'
+        },
+        xAxis: {
+          categories: $scope.providers_names,
+          labels: {
+            rotation: 45
+          }
+        },
+        yAxis: [{
+          title: 'afiliados',
+          opposite: true
+        }],
+        loading: false
+    }
+
+    $scope.reflow = function () {
+      $scope.$broadcast('highchartsng.reflow');
+    };
+  });
+});
