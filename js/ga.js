261d8cc9a1d49f4a39017036e2893e885a8590de
Lucia Escanellas <raviolicode@gmail.com>
2015-02-27 13:07:21 -0200

    Change GA script position

--- js/ga.js
+++ js/ga.js
@@ -1,3 +1,11 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-60196799-1', 'auto');
+ga('send', 'pageview');
+
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N96X92"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -7,3 +15,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N96X92');</script>
 <!-- End Google Tag Manager -->
+
